06928c1950563446b749a939377500e8db5feed9
Dan Hemerlein <danhemerlein@gmail.com>
2025-08-04 20:47:41 -0400

    clean up

--- one-two-8-4.js
+++ one-two-8-4.js
@@ -1,9 +1,9 @@
 const dbank909 = 'RolandTR909'
 const dbank808 = 'RolandTR808'
 setCps(130 / 60 / 4)
-const arp_lpf_sq = slider(1000, 0, 20000)
-const bass_lpf = slider(10000, 0, 20000)
-const first = true
+const arp_lpf_sq = slider(600, 0, 20000)
+const bass_lpf = slider(5000, 0, 20000)
+const first = false
 
 const bass_part = first
   ? `
@@ -43,7 +43,7 @@ const ambiance_part = first
 const hh = s('hh*16').bank(dbank909).gain('0.25')
 const hh_2 = s('white!16')
   .decay(sine.fast(8).range(0.01, 0.05))
-  .gain(0.4)
+  .gain(0.3)
   .sometimesBy(0.1, (x) => x.ply('2 | 4 | 6'))
 const oh = s('[- oh - oh] [<[- hh] -> oh - oh]').bank(dbank909).gain('0.25')
 const bd = s('[bd - - -] [bd - - -] [bd - - -] [bd - - -]')
@@ -58,13 +58,13 @@ const bd_2 = s('bd!16?'.degradeBy(slider(0.749, 0, 1)).ribbon(40, 2))
   .n(irand(5).ribbon(200, 2))
   .bank(dbank909)
 samples('bubo:fox')
+
 const run_2 = n(run(16))
   .s('ftabla')
   .early(2 / 4)
   .sometimes(mul(speed('3'.sometimesBy(1, (x) => x.ply('3 | 1.5 | 2')))))
-  .jux(iter(4))
-  .gain(0.2)
-  ._punchcard()
+// .jux(iter(4)).gain(.2);
+
 // bass
 const bass = note(bass_part)
   .sound('sine')
@@ -127,46 +127,30 @@ const arp_pluck = note(pluck_part)
   .lpf(arp_lpf_sq)
 
 const bg_pattern = stack(
-  short_pluck,
-  short_pluck_two
-  // saw_sidechain,
+  // short_pluck,
+  // short_pluck_two,
+  saw_sidechain
   // ambiance,
 )
 
 const drums = stack(
   // bd,
-  bd_2,
+  // bd_2,
   // sd,
   // sd_2,
-  cp_2
+  cp_2,
   // oh,
-  // hh_2,
-  // run_2,
-  // hh
-  // .phaser(0.3),
+  hh_2,
+  run_2,
+  hh.phaser(0.3)
 )
 // .phaser(0.3)
 // .phaserdepth("<0 .05 .075 .1 .1 .075 .05 0>")
 
-const chord_part = first ? '<E B F# F#>' : '<E B F# C#-7>'
-let chords = chord(chord_part).dict('ireal')
-const mel = stack(
-  chords
-    .offset(-1)
-    .voicing()
-    .s('gm_voice_oohs:4')
-    .vib('128')
-    .phaser(4)
-    .room(1)
-    .rsize(2)
-    .dist(1.25)
-    .lpf(slider(7360, 0, 20000))
-)
-
 samples('github:yaxu/clean-breaks')
 samples('github:algorave-dave/samples/')
 
-const verse_one = s('giveittome')
+const verse = s('giveittome')
   .n(2)
   .chop(16)
   .cut(8)
@@ -174,13 +158,13 @@ const verse_one = s('giveittome')
   .late('[0 .001]*4')
   .late('[0 .001]*2')
   .size(2)
+  .penv('<-1, -1, 2, -1>')
   .phaser(4)
   .room(0.5)
   .sometimesBy(0.5, mul(speed('-1')))
-  .cpm(130 / 4)
-  .lpf(slider(7500, 0, 20000))
+  .lpf(slider(6500, 0, 20000))
 
-const hat_break = s('swat')
+const amen_break = s('swat')
   .fit()
   .chop(16)
   .cut(1)
@@ -190,16 +174,18 @@ const hat_break = s('swat')
   .gain(1.2)
 
 samples('github:eddyflux/crate')
-const crate_loop = stack(
-  s('bd').struct('<[x*<1 2> [~@3 x]] x>'),
-  s('~ [rim:<2 2 4 3>, sd:<2 2 2 3>]').room('<0 .1 0 .2>'),
+const crate_drums = stack(
+  // s("bd").struct("<[x*<1 2> [~@3 x]] x>").gain(1.25).sometimesBy(.1, x => x.ply("2")),
+  s('~ [rim:<2 2 4 3>, sd:<2 2 2 3>]')
+    .room('<0 .1 0 .2 .06 .15 0 .175>')
+    .sometimesBy(0.0, (x) => x.ply('2 | 4'))
   // n("[0 <1 3>]*<2!3 2!3 2!3 4>").s("hh"),
-  s('rd:<1!3 2>*2').gain(0.5)
+  // s("rd:<1!3 2>*2").gain(.5)
+  // s("rd:<2 - - ->").gain(1)
   // s("rd:<1!4>*1").gain(.5)
 )
   .cpm(130 / 2)
   .bank('crate')
-  .sometimesBy(0.0, (x) => x.ply('2 | 4'))
   .sometimesBy(0.0, mul(speed('-1')))
   .late('[0 .01]*4')
   .late('[0 .01]*2')
@@ -208,40 +194,40 @@ const crate_loop = stack(
 
 const melody_chords = chord('<DbM7 EbM7>/4').dict('ireal')
 
+const piano = melody_chords
+  .offset(-2)
+  .voicing()
+  .s('piano')
+  .room(0.5)
+  .rsize(2)
+  .gain(1.25)
+  .cpm(130 / 2)
+
 const pad = melody_chords
   .offset(-1)
   .voicing()
-  .s('gm_epiano1:1')
+  .s('gm_epiano1:2')
   .phaser(4)
   .room(0.5)
+  .gain(1)
   .late('[0 .01]*4')
   .late('[0 .01]*2')
   .size(4)
   .cpm(130 / 2)
 
-const a_bass = n('<0!3 1*2>')
-  .set(melody_chords)
-  .mode('root:g2')
-  .voicing()
-  .s('gm_acoustic_bass')
-  .late('[0 .01]*4')
-  .late('[0 .01]*2')
-  .size(4)
-  .cpm(130 / 2)
-
-const one = stack()
-// drums,
-// bg_pattern,
-// arp_pluck,
-// bass,
-// mel,
+const one = stack(
+  drums,
+  bg_pattern
+  // arp_pluck,
+  // bass,
+).punchcard()
 
 const two = stack(
-  hat_break,
-  crate_loop,
+  amen_break,
+  crate_drums,
   // pad,
-  // a_bass,
-  verse_one
+  // piano,
+  verse
 )
 
 stack(one, two)
