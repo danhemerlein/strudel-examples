29db0f2d64ccb1465ca65415b0e80001e7601586
Dan Hemerlein <danhemerlein@gmail.com>
2026-01-01 20:22:14 -0500

    update 400 coffees

--- real songs/400-coffees.js
+++ real songs/400-coffees.js
@@ -7,36 +7,39 @@ samples('github:yaxu/clean-breaks');
 
 $: s("fdarkkick:2!4").sometimesBy(0.01, x=>x.ply("2"))
   .gain(1.5).duckorbit(2).duckattack(0.1)._scope()
-$: s("~ fohh:3").fast(4).speed(-2).decay(0.1).gain(1.5)._scope()
+$: s("~ fohh:3").fast(4).speed(-2).decay(0.08)
+  .sometimesBy(0.05, x=>x.ply("2")).gain(1.5)._scope()
 
 
 _$: s("swat").fit().chop(16).cut(1)
   .sometimesBy(.5, mul(speed("0.9")))
   .gain(.75).lpf(3500)._scope()
-$: s("~ fsnare:2").fast(2).decay(0.1).gain(0.5)
+_$: s("~ fsnare:2").fast(2).decay(0.1).gain(0.5)
 
-$: s("400-coffees").slow(8).speed("-1|2")
-   .slice(16, `< ~ [~ 4] ~ [~ 2]>`)
+// i'd like to know if i can restrict the speed backwards to only the [~ 4] parts of the extended pattern
+_$: s("400-coffees").slow(8).sometimesBy(.5, x=>x.speed("-1"))
+   // .slice(16, `< ~ [~ 4] ~ [~ 2]>`)
+    .slice(16, `<
+    ~ [~ 4] ~ [~ 2]
+    [3!2] [~ 4] [5!2] ~
+    [12 13] [12 13] [2 3] [~ 4]
+    [5!2] [6 7] [5!2] [~ 4]
+    >`)
    .hpf(1000)
    .orbit(2)
-   .gain(2).room(1)._punchcard()
-$: s("400-coffees").slow(8)
-  .slice(16, `
-  <[2 3] [2 3] [2 3] [~ 4]
-   [5!2] [6 7] [5!2] [~ 4]
-  [12 13] [12 13] [2 3] [~ 4]
-  [5!2] [6 7] [5!2] [~ 4]>`)
-  .hpf(1000)
-  .delay(.2)
-  .orbit(2)
-  .gain(2).room(0.5)._punchcard()
+   .gain(1.5).room(1)._punchcard()
 
-_$: s("400-coffees").slow(8).gain(1.25)._scope()
-_$: s("400-coffees:1").slow(8).gain(1).room(1)._scope()
+_$: s("400-coffees").slow(8)
+     .slice(8, `<0 1 2 3 4 5 6 7 8>`)
+   // .slice(32, `<0 ~ ~ 0 ~ ~ [0 0] ~ ~ 24 ~ ~ 24 ~ 25 [~ 25]>`).fast(4)
+  // .slice(32, `<[~ 25] ~ ~ [~ 25] ~ ~ [~ 25] ~ ~ [~ 25] ~ ~ [~ 25] ~ [~ 25] [~ 25]>`).fast(4)
+  .gain(1.25)._scope()
+_$: s("400-coffees:1").slow(8).slice(8, `<0 1 2 3 4 5 6 7 8>`).gain(1).room(1)._scope()
 
 $: s("wt_digital_echoes")
     .n("3"
-       .add("<0 -3 2 -3>")
+       // <0 -3 [-2|2] -3>
+       .add("<0 -3>")
        .add("[0, 2, 4, 6]"))
     .scale("d:major")
     .transpose(-12)
@@ -51,13 +54,13 @@ $: s("wt_digital_echoes")
     .orbit(2)
     // .lpf("<4000@2 2000@2 800@2 400@2>")
     // .gain("<1@2 0.7@2 0.4@2 0.1@2>")
-    .gain(2)
+    .gain(1.2)
     .slow(2)
     ._pianoroll({
       labels: 1
     })
 
-$: note("{3 ~ ~ 3 ~ 3 ~ ~ 5 ~ ~ 3 ~ 5 ~ 3}%16"
+_$: note("{3 ~ ~ 3 ~ 3 ~ ~ 5 ~ ~ 3 ~ 5 ~ 3}%16"
       .add("<0 -3 -2 -3>")
       .add("[0, 2, 4, 6]")
     .scale("d:major"))
@@ -77,15 +80,15 @@ $: note("{3 ~ ~ 3 ~ 3 ~ ~ 5 ~ ~ 3 ~ 5 ~ 3}%16"
     // .lpf("2000")
     // .hpf("50")
     .delay(0.2)
-    .slow(2)
+    // .slow(2)
     ._pianoroll({ labels: 1 })
 
 
 
 // "<g1:major:pentatonic d1:major:pentatonic e1:minor:pentatonic d1:major:pentatonic>"
 
-_$: n(irand(8).seg(8))
-    .scale("<g1:major:pentatonic d1:major:pentatonic e1:minor:pentatonic d1:major:pentatonic>")
+$: n(irand(8).seg(8))
+    .scale("<g1:major:pentatonic d1:major:pentatonic>")
     .s("recorder_bass_sus")
     .rib(4,4)
     .orbit(2)
@@ -96,7 +99,11 @@ _$: n(irand(8).seg(8))
       labels: 1
     })
 
-$: n("<3 0 1 0>").scale("d2:major")
+
+
+$: n("<3 0>")
+    .scale("d2:major")
+    // .struct("x _ _ _ _ _ _ _ _ _ x _ _ _ _ _")
     .s("sine")
     .gain(1.3)
     .slow(2)
