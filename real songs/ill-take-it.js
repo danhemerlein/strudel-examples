6aea39266e309c3035f86bc9d67610cea60d0b93
Dan Hemerlein <danhemerlein@gmail.com>
2026-01-02 09:59:07 -0500

    begin ill take it cover

---
+++ real songs/ill-take-it.js
@@ -0,0 +1,8 @@
+// I'll take it
+
+setCps(130/60/4)
+
+
+$: s("bd*4").bank("RolandTR909").duckorbit(2).duckattack(0.1)
+$: s("[~ cp]").fast(2).bank("RolandTR909")
+$: n("0!8 0 2!3 1!8 1 -1!3").slow(4).scale("c2:major").s("supersaw, sine").orbit(2)
