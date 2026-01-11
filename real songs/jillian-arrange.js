8bfddecc7b7900099bbd08e572a3c73c3dae90e6
Dan Hemerlein <danhemerlein@gmail.com>
2026-01-11 12:31:21 -0500

    update jillian arrange and voicing

--- real songs/jillian-arrange.js
+++ real songs/jillian-arrange.js
@@ -1,18 +1,25 @@
 setCps(130/60/4)
-samples('github:danhemerlein/samples')
-samples('bubo:fox')
 
-const synth_energy = slider(2115.5, 500,5000)
+samples('github:danhemerlein/samples')
+samples('github:kyrsive/glorkglunk-wavetables')
+samples('github:kyrsive/diarrhea-drumkit')
+samples('github:bindbindbind/miscsamples/main/glitch_with_friends')
+samples('github:mmmgarlic/randumsample')
+samples('github:emptyflash/samples')
+
+const synth_energy = slider(2291, 500, 5000)
+const bass_energy = slider(586, 100, 1000)
+const drums_energy = slider(1994, 500, 5000)
 const chop_energy = mul(synth_energy, 1.25)
 
 // code by switch angel
-
 register('sf', (cycles,x) => {
   const segments = 16;
   cycles = reify(cycles)
   return x.scrub(saw.seg(cycles.mul(segments)).slow(cycles))
 })
 
+// code by DJ Dave
 const notes_2 = [
   "{e3 b4 ~}%2",
   "{b3 f#4 ~}%2",
@@ -20,13 +27,14 @@ const notes_2 = [
   "{c#3 g#3 ~}%2",
 ]
 
-const bd = s("bd!4").bank("r8").gain(2).dec(0.9).fast("1 1 1 1").duckorbit(2).duckattack(0.1)
-
-const oh = s("[~ oh:2]").bank("tr808").fast(4).speed(perlin.range(0.8,1.2)).decay(0.2).room(0.2).gain(.4)
+const bd = s("bd!4").bank("r8").gain(2).dec(0.9).fast("1 1 1 1").duckorbit(2).duckattack(0.1).lpf(drums_energy);
+const oh = s("[~ oh:2]").bank("tr808").fast(4).speed(perlin.range(0.8,1.2)).decay(0.2).room(0.2).gain(.4).lpf(drums_energy);
 
 const bass = note("<e1 b1 f#1 c#1>").s("supersaw")
-  .seg(8).sometimesBy(0.1, x=>x.seg(16)).orbit(2)
-  .lpf(500).lpq(7).distort("2.5:.7")
+  .seg(8)
+  //.sometimesBy(0.1, x=>x.seg(16))
+  .orbit(2)
+  .lpf(bass_energy).lpq(7).distort("2.5:.7")
   .ftype('ladder').gain(1.25)
 
 const hi = note("<d#4(3,8,0)!2 [b4(3,8,0)!1|d#4(4,16,16)|d#4(8,16,16)e4(3,16,16)]>")
@@ -37,17 +45,17 @@ const hi = note("<d#4(3,8,0)!2 [b4(3,8,0)!1|d#4(4,16,16)|d#4(8,16,16)e4(3,16,16)
   .lpf(synth_energy)
   .room(1.1).delay(1.25)
 
+// code by DJ Dave
 const main_synth = note(pick("<0!8 1!8 2!8 3!8>", notes_2))
 .fast(8)
 .sound("square, saw")
-// .degradeBy(0.2)
-// .rib(4,4)
 .lpf(synth_energy)
 .gain(0.4)
 .sometimes(x=>x.transpose("[0, 7, 12]"))
 .decay(0.2).rarely(x=>x.decay("[0.2|0.3|0.4]"))
 .delay(0.25)
-.room("[0.5|0.75|1]".fast(2))._punchcard()
+.room("[0.5|0.75|1]".fast(2))
+
 const chop = s("youngandnauseous:1").slow(4).gain(2.5).room(2.5).rsize(.5).delay(.2)
 .chop(16).cut(1)
 .sometimesBy(.5, ply("2"))
@@ -56,35 +64,31 @@ const chop = s("youngandnauseous:1").slow(4).gain(2.5).room(2.5).rsize(.5).delay
 const intro = s("youngandnauseous:4").slow(4).gain(2).room(1).rsize(1).delay(.2).lpf(chop_energy)
   .scrub(irand(8).div(8).seg(8).speed(2).degradeBy(0.2)
          .sometimesBy(.25, ply("2"))
-         .rib("11", 2)).sf("2")
+         .rib("11", 2))//.sf("2")
 
-const verse = s("youngandnauseous").slow(4).gain(3).slice(8, "0 1 2 3")
-  .slow(8).transient(-1).room(1)
+const verse = s("youngandnauseous").slow(4).gain(2).slice(8, "0 1 2 3").slow(8)
+const verse_2 = s("youngandnauseous").slow(4).gain(2).slice(8, "4 5 6 7").slow(8)
 
-const verse_2 = s("youngandnauseous").slow(4).gain(3).slice(8, "4 5 6 7")
-  .slow(8).transient(-1).room(1)
-
-const hat = s("hh").ply(16).stretch(0).lfo({da:2}).postgain("[18 16 18 16] [18 16 16 16]").gain(.2)
-
-let riser = s("gwf_im:24").speed(-3.7).postgain(1.9)
+const hat = s("hh!16").stretch(0).lfo({da:2}).postgain("[18 16 18 16] [18 16 16 16]").gain(.1)//.transient(1);
+const riser = s("gwf_im:24").speed(-3.7).postgain(1.9);
+const HEY = s("impact_femmebot").slow(16).postgain(8).speed(1.5).gain(.5)
+const bd_oh = stack(bd, oh)
 
 $: arrange(
-  [2, stack(bd, oh)],
-  [2, stack(bd, oh, intro)],
-  [2, stack(bd, oh, intro, hi)],
-  [4, stack(bd, oh, intro, hi, bass)],
+  [2, stack(bd_oh)],
+  [4, stack(bd_oh, intro)],
+  [4, stack(bd_oh, intro, hi)],
+  [8, stack(bd_oh, intro, hi, bass)],
   [8, stack(bass, verse)],
   [8, stack(bd, bass, verse_2, hi)],
   [8, stack(bass, main_synth, chop)],
-  // , riser, intro, oh, hat
-  // [3, stack(main_synth, hi).room(2)],
-  // [1, stack(bd, riser, oh, hi).room(1.5)],
-  // [8, stack(bd, oh, hat, intro, bass, main_synth, hi)],
-  // [8, stack(bd, oh, hat, bass, main_synth)],
-  // [8, stack(bd, oh, hat, intro, bass, main_synth, hi)],
-)
+  [7, stack(main_synth, hi, chop).room(2).rsize(1).lpf(5000).hpf(400).delay(.2)],
+  [1, stack(riser).room(1.5).rsize(4)],
+  [32, stack(HEY, bd_oh, hat, bass, main_synth)],
+).master()
 
+const mastering = register('master', (pat) => pat.bus(1).dry(0))
 
 $: s("bus:1")
-  .compressor("-10:20:10:.002:.02")
+  // .compressor("-10:20:10:.002:.02")
   .soft("1:1")._scope()
--- scratch/voicing.js
+++ scratch/voicing.js
@@ -1,10 +1,10 @@
 
 $: setcps(170/60/4)
 
-$: s("bd(3,8)").bank("tr909").color("white").duck(2)
-  .duckatt(.5).duckdepth(.75).duckons(.01)
+$: s("bd(3,8)").bank("tr909").duck(2).duckatt(.5).duckdepth(.75).duckons(.01)
+$: s("white").att(0.2).slow(8).lpf(20000).gain(.05).delay(.2).room(1).orbit(2)
 
-$: chord("<C Em G Am>/2")
+$: chord("<G D Em D>/2")
   .voicing()
   .orbit(2)
   .slow(2)
@@ -15,18 +15,18 @@ $: chord("<C Em G Am>/2")
   .superimpose(x=>x.transpose(-12))
   .superimpose(x=>x.transpose(12))
   .superimpose(x=>x.lpf(1000))
-  .lpf(slider(3790,0,10000))
-  .lpf(sine.fast(slider(9.569, 1, 20))
-       .mul(slider(4550, 0, 10000))
-       .add(slider(1820, 0, 10000))
+  .lpf(slider(6790,0,10000))
+  .lpf(sine.fast(slider(13.825, 1, 20))
+       .mul(slider(8570, 0, 10000))
+       .add(slider(6380, 0, 10000))
   )
   .lpq(10)
   .transpose(-12)
-  .gain(.1)
+  .attack(0.5)
   .decay(4)
   .release(2)
-  .sound("pulse")
-  .color("lightblue")
+  .sound("sine")
+  .gain(.06)
   ._pianoroll()
 
 let rhythm = "[24 <36 44>]*4"
@@ -35,8 +35,9 @@ let rhythm = "[24 <36 44>]*4"
   .sometimesBy(.05, ply(2))
   .lpf(sine.range(2400, 3200).segment(7).slow(2))
   .pan(sine.range(.1, .9).slow(3))
+  .speed(1.25)
   .s("conga:13")
-  .color("beige")
+
 
 let high = "-@3 [- <67>]"
   .sometimesBy("<.1 .3@2>", add(1))
@@ -44,7 +45,6 @@ let high = "-@3 [- <67>]"
   .lpf(3000)
   .pan(sine.range(.4, .6).slow(7))
   .s("conga").n("<13@2 18 19>")
-  .color("orange").gain(2)
 
 let chorus = "40 50@2 [32 -]".add("<0@2 2>")
     .off(perlin.range(1/256, -1/256), x=>x.add(-1))
@@ -61,10 +61,3 @@ $: stack(
 )
 .room(perlin.range(.8, 1.2).segment(17)).roomsize(3).gain(1.5)
 
-$: n("<- <[0 .. 31] [31 .. 0]> - ->")
-.scale("<c1 f1>/2:minor:pentatonic")
-.dec(.15).room(.2).delay(.5)
-.sometimesBy(1, x=>x.coarse("4"))
-.postgain(.4).s("sine").fm(3)
-.lpf(sine.range(1500,3000).slow(8))
-.add(note(perlin.range(0,.5))).fmh(5.1)
