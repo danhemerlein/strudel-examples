9e8ab182dd5da424270c96d49e7ccd392b21b604
Dan Hemerlein <danhemerlein@gmail.com>
2026-04-04 18:36:22 -0400

    add vibe to jillian

--- real songs/jillian-arrange.js
+++ real songs/jillian-arrange.js
@@ -4,9 +4,9 @@ samples('github:danhemerlein/samples')
 samples('github:mmmgarlic/randumsample')
 samples('github:bindbindbind/miscsamples/main/glitch_with_friends')
 
-const synth_energy = slider(3488, 500, 5000)
-const bass_energy = slider(516.45, 50, 1000)
-const drums_energy = slider(2005.3, 100, 3000)
+const synth_energy = slider(2630, 0, 5000)
+const bass_energy = slider(304, 0, 1000)
+const drums_energy = slider(2058, 0, 3000)
 const chop_energy = mul(synth_energy, 1.25)
 
 // code by switch angel
@@ -27,6 +27,10 @@ const notes_2 = [
 const bd = s("kick3_afterparty!4").gain(4)
   .fast("1 1 1 <1 1 1 2>").duckorbit(2).duckattack(0.1).lpf(drums_energy);
 
+const bd_double = s("kick3_afterparty!4").gain(4)
+  .fast("<1@8 2@2 4@2>")
+  .lpf(drums_energy);
+
 const oh = s("[~ hat_drugs]").fast(4)
   .speed(perlin.range(0.8,1.2)).decay(0.2).room(0.2).gain(8).lpf(drums_energy);
 
@@ -45,7 +49,6 @@ const bass = note("<e1 b1 f#1 c#1>").s("supersaw")
   .seg(8)
   .orbit(2)
   .lpf(bass_energy).lpq(7).distort("2.5:.7")
-  // .vowel("i")
   .ftype('ladder').gain(1)
 
 const hi = note("<d#4(3,8,0)!2 [b4(3,8,0)!1|d#4(4,16,16)|d#4(8,16,16)e4(3,16,16)]>")
@@ -61,7 +64,7 @@ const hi = note("<d#4(3,8,0)!2 [b4(3,8,0)!1|d#4(4,16,16)|d#4(8,16,16)e4(3,16,16)
 const main_synth = note(pick("<0!8 1!8 2!8 3!8>", notes_2))
   .fast(8)
   .sound("square, saw").delay(1.25)
-  .sometimes(x=>x.transpose("[0, 7, 12]"))
+  .sometimes(x=>x.transpose("[0, 7, 12, 24]"))
   .decay(0.1).sometimesBy(1, x=>x.decay("[0.02|0.03|0.04]"))
   .delay(0.25)
   .room(1.2)
@@ -83,43 +86,42 @@ const synth_topline = [
 const hi_2 = note(pick("<0!4 1 2 1 3>", synth_topline))
   .slow(2)
   .sound("pulse,square,wt_digital:3")
-  .almostAlways(x => x.ply(2))
+  .ply(2)
   .transpose("12")
   .decay(0.1).sometimesBy(1, x=>x.decay("[0.1|0.07|0.08]"))
   .delay(0.25)
   .room(1.2)
-  .gain(0.6)
+  .gain(0.5)
   .orbit(2)
   .lpf(synth_energy)
 
-const chop = s("youngandnauseous:1").slow(4).gain(4)
-.delay(.2)
-.chop(16).cut(1)
-.sometimesBy(.5, ply("2"))
-.sometimesBy(.25, mul(speed("-1"))).ribbon("[11|7|15]", 1).hpf(500)
-
-// const chop = s("youngandnauseous:2").slow(4).gain(4)
+// const chop = s("youngandnauseous:1").slow(4).gain(4)
 // .delay(.2)
 // .chop(16).cut(1)
 // .sometimesBy(.5, ply("2"))
 // .sometimesBy(.25, mul(speed("-1")))
-// .ribbon("[8|9|11]", 1).hpf(500)
+// .ribbon("[11|7|15]", 1)
+// .lpf(chop_energy)
+// .hpf(500)
 
-// const chop = s("youngandnauseous:3").slow(4).gain(4)
+// const chop = s("youngandnauseous:2").slow(4).gain(4)
 // .delay(.2)
-// .chop(16).cut(1)
-// // .ply("2")
-// // .mul(speed("-1"))
-// .ribbon("[4|5|6]", 1).hpf(500)
+// .scrub(irand(8).div(8).seg(8).degradeBy(0.2))
+//   .sometimesBy(.2, ply("2"))
+//   .sometimesBy(0.8, mul(speed("2")))
+// .ribbon("2", 2)
+// .hpf(500)
+// .lpf(chop_energy)
+// ._punchcard()
 
 // const chop = s("youngandnauseous:4").slow(4).gain(2)
 // .delay(.6)
 // .chop(16).cut(1)
-// // .ply("2")
 // .mul(speed("2"))
 // .ribbon("[4]", 1).hpf(500)
+// .lpf(chop_energy)
 
-const intro = s("youngandnauseous:4").slow(4).gain(2).room(1).rsize(1).delay(.2).lpf(chop_energy)
+const intro = s("youngandnauseous:4").slow(4).gain(1.5).room(1).rsize(1).delay(.2).lpf(chop_energy)
   .scrub(irand(8).div(8).seg(8).speed(2).degradeBy(0.2)
          .sometimesBy(.25, ply("2"))
          .rib("11", 2)).sf("2")
@@ -137,7 +139,7 @@ const bd_oh = stack(bd, oh)
 const mastering = register('master', (pat) => pat.bus(1).dry(0))
 
 $: arrange(
-  [2, stack(bd_oh)],
+  [4, stack(bd_oh)],
   [4, stack(bd_oh, hi)],
   [4, stack(bd_oh, hi, intro)],
   [8, stack(bd_oh, hi, intro, bass_verse)],
@@ -145,21 +147,21 @@ $: arrange(
   [8, stack(bass_verse)], // first verse
   [8, stack(bd, bass, hi)], // first verse
 
-  [8, stack(bd, bass, main_synth)], // chorus
-  [7, stack(bd_oh, bass, main_synth, hi)], // chorus
-  [1, stack(bd, riser, clicks, chop).room(1.5).rsize(2).rfade(0.5)], // transition
+  [8, stack(bd_double, bass, main_synth)], // chorus
+  [7, stack(bd_double, oh, bass, main_synth, hi)], // chorus
+  [1, stack(riser, clicks).room(1.5).rsize(2).rfade(0.5)], // transition
 
+  [8, stack(impact, bd_oh, bass, hi_2, energy_hat)], // drop
   [16, stack(impact, bd_oh, bass, hi_2, energy_hat, chop)], // drop
 
   [8, stack(bass, hi)], // second verse
 
-  [8, stack(bd, bass)], // chorus
-  [7, stack(bd, bass, hi, back_beat)], // chorus
-  [1, stack(riser, clicks, chop).room(1.5).rsize(2).rfade(0.5)], // transition
-  [16, stack(impact, bd_oh, bass, hi_2, energy_hat, back_beat)], // drop
+  [8, stack(bd_double, bass, main_synth)], // chorus
+  [7, stack(bd, bass, hi, back_beat, main_synth)], // chorus
 
-  // [4, stack(bd_oh, bass, intro, main_synth, hi)], // drop
-  // [32, stack(impact, bd_oh, intro, hi, bass, main_synth)], // outro
+  [1, stack(riser, clicks).room(1.5).rsize(2).rfade(0.5)], // transition
+  [8, stack(impact, bd_oh, bass, hi_2, energy_hat, back_beat)], // drop
+  [16, stack(impact, bd_double, oh, bass, hi_2, energy_hat, back_beat, intro, chop)], // outro
 
 ).master()
 
