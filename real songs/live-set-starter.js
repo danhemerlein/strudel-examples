6e31b318d70736280c822d3b770504b86960be34
Dan Hemerlein <danhemerlein@gmail.com>
2026-04-27 21:11:43 -0400

    update live set starter

--- real songs/live-set-starter.js
+++ real songs/live-set-starter.js
@@ -2,10 +2,10 @@
 // mood ring - 94
 // four hundred coffees - 125
 
-setcpm(94/4)
+setcpm(130/4)
 samples('github:danhemerlein/samples')
 
-const lpf = slider(500, 0, 10000)
+const lpf = slider(4580, 0, 10000)
 
 const synth_topline = [
   `c#4 b3 g#3 c#4 b3 g#3 c#4 b3
@@ -18,12 +18,20 @@ const synth_topline = [
   a#3 g#3 g#3 f#3 f#3 e3 e3 d#3`
 ]
 
-_$: s("youngandnauseous:1").slow(4)
+$: s("youngandnauseous:1").slow(4).chop(16).cut(1)
+.sometimesBy(.5, ply("2"))
+.sometimesBy(.25, mul(speed("-1")))
+.ribbon("[11|7|15]", 1)
+.delay(.25)
+.lpf(lpf)
+.hpf(1000)
+.gain(3)
+
 _$: s("moodring:2").slow(4)
-_$: s("400-coffees:1")
-  .slow(8).slice(8, "<0 ~ 2 ~ 4 ~ 6 ~>")
 
 all(x=> x
   .crush(10)
-  // .lpf(5000).hpf(500)
-  ._scope())
+  // .lpf(1000).hpf(500)
+    ._scope()
+    // .gain(0)
+  )
