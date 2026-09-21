94b18204bc3534a73d4d458c53e6cccec854e2b0
Dan Hemerlein <danhemerlein@gmail.com>
2026-09-20 20:25:26 -0400

    add bass

--- real songs/molly-in-the-club.js
+++ real songs/molly-in-the-club.js
@@ -27,25 +27,36 @@ const layer =  note("d4@2 [e4 [e4 f#4]]@2".slow(4)).sound("gm_synth_strings_1:4"
 const hi = note("d5")
   .struct("x ~ x ~ ~ ~ ~ x ~ x ~ x ~ x ~ ~")
   .s("supersaw,wt_digital_echoes")
+  .attack(.05)
+  .clip(.8)
   .crush("6")
-  .delay(.75)
-  .room("1.1")
+  .delay(.25)
+  .room(1.1)
   .pan("<.5 .3 .5 .8>")
   .postgain(.7)
-  .lpf(sine.range(500,2000))
+  .lpf(sine.range(1000,3000).slow(2))
 
-// const trem = note("d4@2 [e4 [e4 f#4]]@2".slow(4)).sound("gm_tremolo_strings").transpose("0,12")
-
+const bass = note("<g1 b1 a1 [d2 a1]>").s("supersaw")
+  .seg(8)
+  .lpf(1000)
+  .lpq(7).distort("2.5:.7")
+  .ftype('ladder')
+  .clip(.95)
+  .glide(.1)
+  .postgain(1.5)._pianoroll({labels: true})
 const verse = stack(bd, sd, sh, sd_2, oh, tm, cp, pad, layer)
 const pre = stack(hi, bd, sd, sh, sd_2, oh, tm, cp, pad, layer)
+const chorus = stack(hi, bd, sd, sh, sd_2, oh, tm, cp, pad, layer, bass)
 
 const mastering = register('master', (pat) => pat.bus(1).dry(0))
 
 $: arrange(
   [16, verse],
-  [16, pre],
+  [4, pre],
+  [16, chorus],
+  // [4, hi]
 ).master()
 
 $: s("bus:1")
   .compressor("-10:4:20:.01:.15")
-  .soft("1:1").gain(0.15)._scope()
+  .soft("1:1").gain(0.2)._scope()
